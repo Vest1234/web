f55ff53ecdd625f14d3e1ea4c7438ec9fd61da12
Tomyum1 <fuitorsil@gmail.com>
2023-02-12 19:58:35 +0700

    12 февраля

---
+++ lesson_20/js.js
@@ -0,0 +1,47 @@
+// function logger() {
+//     console.log('Всем привет!');
+// }
+
+// logger () ;
+// logger () ;
+
+// function juiceMaker (apples, oranges) {
+//     console.log(apples, oranges);
+//     const juice=`Ваш сок из $(apples) яблок и $(oranges) апельсинов готов!`
+//     return juice
+// }
+// function juiceMaker (apples, oranges) {
+//     oranges = oranges || "без использования"
+//     console.log(apples, oranges)
+//     const juice
+// }
+
+// let dev1 = 44;
+// let dev2 = 23;
+// let dev3 = 71;
+
+// let des1 = 65;
+// let des2 = 54;
+// let des3 = 49;
+
+// function srar1 (dev1, dev2, dev3, des1, des2, des3){
+//    da = dev1 + dev2 + dev3;
+//    ds = (da / 3)
+
+//    a = (des1 + des2 + des3 )
+//    fs = (a / 3)
+
+// if (ds > fs){
+//     alert("Dev победила!")
+// }else{
+//     alert(`Des победила! С разницей ${fs - ds} очка.`)
+// }
+// }
+// srar1(dev1, dev2, dev3, des1, des2, des3);
+
+// const students = ["1", "2", "3"]
+// // console.log(students[0]);
+// console.log(students.length)
+// students[3]="4"
+// console.log(students);
+
