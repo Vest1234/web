a3202339105f91a82d9425316e94db2a5f8c8f16
lexasd <atomilov720@gmail.com>
2023-02-26 19:54:50 +0700

    26 февраля

---
+++ lesson_22/js.js
@@ -0,0 +1,55 @@
+// for( let jump=1 ; jump<=10 ; jump++){
+//     console.log("Прыжок " + jump)
+// } 
+
+// let l = ["Прив", 7, [1,2,3] ,{6: "kkk"} ]
+//  for( let un = 0; un < l.length ; un++){
+//      console.log(l[un])
+//  } 
+     
+// console.log(typeof  )  
+
+// let p = [1920 , 2013 , 2020 , 1996 , 1630]
+// let p1 = []
+// for (let i = 0; i < p.length; i++) {
+//     p1.push(2023 - p[i])
+
+// }
+// console.log(p1)
+
+// const g = [1920 , 2013 , 2020 , 1996 , 1630]
+// for( let jump = 0 ; jump>= 0 ; jump--){
+//     console.log("Число " + jump)
+//     e 
+// } 
+// for( let jump=1 ; jump<=10 ; jump++){
+//     if (jump == 4){
+//         break;
+//     }
+//     console.log("Прыжок " + jump)
+// } 
+
+// for( let jump=1 ; jump<=10 ; jump++){
+//     console.log("Прыжок " , jump)
+//     for( let s=1 ; s<=3 ; s++)
+//     console.log("Присед " , s)
+
+
+// // }
+// let x = 1;
+// while (x < 10) {x++
+//     console.log("Прыжок " + x)
+// }
+
+// console.log(x);
+
+// while(true){
+//     let a = prompt("Введи число")
+//     if (a > 10){
+//         console.log("Введено больше 10")
+//     }else{
+//         console.log("Good")
+//         break;
+//     } 
+// }
+//100  слайд
